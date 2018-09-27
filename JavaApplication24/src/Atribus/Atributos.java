1d51901f184d7c2f3c27d541dae54df927e168c5
JohanBenavides <43528598+JohanBenavides@users.noreply.github.com>
2018-09-27 17:40:29 -0500

    Add files via upload

---
+++ JavaApplication24/src/Atribus/Atributos.java
@@ -0,0 +1,27 @@
+/**
+ * Este es mi primer mi primer proyecto con objetos en java
+ * vamos a usar los atributos para enviar datos
+ **/
+package Atribus;
+
+/**
+ *
+ * @author Johan Benavides
+ * septiembre 27 de 2018
+ */
+public class Atributos {
+
+ /**
+  * @param args the command line arguments
+  */
+ public static void main(String[] args) {
+  // Aqui creamos las acciones y los objetos
+  
+  caracteristicas bici1 = new caracteristicas();
+  
+  bici1.Verdatos();
+  
+  
+ }
+ 
+}
---
+++ JavaApplication24/src/Atribus/caracteristicas.java
@@ -0,0 +1,31 @@
+/**
+ * aqui vamos a crear los atributos de los objetos
+ * para que despues sean mostrados por cada objeto
+ */
+package Atribus;
+
+/**
+ *
+ * @author Johan Benavides
+ * 27 de septiembre de 2018
+ */
+public class caracteristicas {
+ //Aqui crearemos los atributos
+ 
+ String manubrio="Scott", grupo="Shimano", marco="Montabike", marca="trek", modelo="2017", sillin="colle";  
+	 int ruedas=3;
+	 
+	 public void Verdatos (){
+ 
+   System.out.println("Los datos de la bicicleta son:");
+   System.out.println("\nLa bici tiene un manubrio: "+manubrio+"\n");
+   System.out.println("\nLa bici tiene un grupo: "+grupo+"\n");
+   System.out.println("\nLa bici tiene una marca "+marca+"\n");
+   System.out.println("\nLa bici tiene un modelo: "+modelo+"\n");
+   System.out.println("\nLa bici tiene un sillin: "+sillin+"\n");
+   System.out.println("\nLa bici tiene: "+ruedas+" ruedas\n");
+   
+   
+	 
+   }	 
+}
