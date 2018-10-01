d925e77abe4848aa1510aad3ce22cb518ee3300f
JohanBenavides <43528598+JohanBenavides@users.noreply.github.com>
2018-10-01 14:33:01 -0500

    Add files via upload

---
+++ objetos.py
@@ -0,0 +1,109 @@
+"""
+Ester es mi primer proyecto con phyton creando atributos objetos
+vamos a asignar atributos a los obhjetos que creemos
+Creafo por Johan Benavides Arias
+01 de octubre de 2018
+
+class Nombre_Clase:
+    pass
+
+"""
+class Bicicleta:
+    pass
+"""
+aqui vamos a crear nuestro primer objeto
+
+objeto=nombre_clase()
+"""
+monareta=bicicleta()
+cross=bicicleta()
+fixie=bicicleta()
+mountainbike=bicicleta()
+panadera=bicicleta()
+
+
+"""
+en esta parte crearemos los atributos de las bicicletas
+nombre_objeto.nombre_del_atributo=valor
+"""
+monareta.manubrio="Horrible"
+monareta.marca="Monareta"
+monareta.modelo=1972
+monareta.ruedas=2
+monareta.sillin="feo y con espaldar"
+monareta.marco="Sin barra"
+
+cross.manubrio="Cross aluminio"
+cross.marca="Cross"
+cross.modelo=2015
+cross.ruedas=2
+cross.sillin="bajo"
+cross.marco="bmx"
+
+fixie.manubrio="Zoom curvo"
+fixie.marca="GW"
+fixie.modelo=2016
+fixie.ruedas=2
+fixie.sillin="aerodimanico"
+fixie.marco="Innova"
+
+mountainbike.manubrio="Horrible"
+mountainbike.marca="Monareta"
+mountainbike.modelo=1972
+mountainbike.ruedas=2
+mountainbike.sillin="feo y con espaldar"
+mountainbike.marco="Sin barra"
+
+panadera.manubrio="Horrible"
+panadera.marca="Monareta"
+panadera.modelo=1972
+panadera.ruedas=2
+panadera.sillin="feo y con espaldar"
+panadera.marco="Sin barra"
+
+print("Estos son los datos de mi bicicleta monareta")
+print("El manubrio de mi bicicleta es: " , monareta.manubrio)
+print("La marca de mi bicicleta es: " , monareta.marca)
+print("El modelo de mi bicicleta es: " , monareta.modelo)
+print("El numero de ruedas de mi bicicleta son: " , monareta.ruedas)
+print("El marco de mi bicicleta es: " , monareta.marco)
+print("El sillin de mi bicicleta es: " , monareta.sillin,"\n\n")
+
+
+print("Estos son los datos de mi bicicleta cross")
+print("El manubrio de mi bicicleta es: " , cross.manubrio)
+print("La marca de mi bicicleta es: " , cross.marca)
+print("El modelo de mi bicicleta es: " , cross.modelo)
+print("El numero de ruedas de mi bicicleta son: " , cross.ruedas)
+print("El marco de mi bicicleta es: " , cross.marco)
+print("El sillin de mi bicicleta es: " , cross.sillin,"\n\n")
+
+
+print("Estos son los datos de mi bicicleta fixie")
+print("El manubrio de mi bicicleta es: " , fixie.manubrio)
+print("La marca de mi bicicleta es: " , fixie.marca)
+print("El modelo de mi bicicleta es: " , fixie.modelo)
+print("El numero de ruedas de mi bicicleta son: " , fixie.ruedas)
+print("El marco de mi bicicleta es: " , fixie.marco)
+print("El sillin de mi bicicleta es: " , fixie.sillin,"\n\n")
+
+
+print("Estos son los datos de mi bicicleta mountainbike")
+print("El manubrio de mi bicicleta es: " , mountainbike.manubrio)
+print("La marca de mi bicicleta es: " , mountainbike.marca)
+print("El modelo de mi bicicleta es: " , mountainbike.modelo)
+print("El numero de ruedas de mi bicicleta son: " , mountainbike.ruedas)
+print("El marco de mi bicicleta es: " , mountainbike.marco)
+print("El sillin de mi bicicleta es: " , mountainbike.sillin,"\n\n")
+
+
+print("Estos son los datos de mi bicicleta panadera")
+print("El manubrio de mi bicicleta es: " , panadera.manubrio)
+print("La marca de mi bicicleta es: " , panadera.marca)
+print("El modelo de mi bicicleta es: " , panadera.modelo)
+print("El numero de ruedas de mi bicicleta son: " , panadera.ruedas)
+print("El marco de mi bicicleta es: " , panadera.marco)
+print("El sillin de mi bicicleta es: " , panadera.sillin,"\n\n")
+
+
+
